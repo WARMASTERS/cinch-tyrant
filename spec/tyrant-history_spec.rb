4268f1013da24acc1fd688d6aee11c6d390607ac
Peter Tseng <pht24@cornell.edu>
2014-04-27 00:47:38 +0000

    spec: Don't expect getOldFactionWar in TyrantHistory init

--- spec/tyrant-history_spec.rb
+++ spec/tyrant-history_spec.rb
@@ -15,7 +15,13 @@ describe Cinch::Plugins::TyrantHistory do
     @conn = FakeConnection.new
     @tyrant = Tyrants.get_fake('testplayer', @conn)
     allow(Tyrants).to receive(:get).with('testplayer').and_return(@tyrant)
+  end
 
+  it 'makes a test bot' do
+    expect(bot).to be_a Cinch::Bot
+  end
+
+  it 'shows old wars' do
     @conn.respond('getOldFactionWars', nil, {'wars' => [
       {
         'faction_war_id' => '1',
@@ -28,13 +34,7 @@ describe Cinch::Plugins::TyrantHistory do
         'def_pts' => '0',
       },
     ]})
-  end
 
-  it 'makes a test bot' do
-    expect(bot).to be_a Cinch::Bot
-  end
-
-  it 'shows old wars' do
     msg = make_message(bot, '!last', channel: '#test')
     replies = get_replies_text(msg)
     expect(replies).to be == ['         1 - faction 1000 vs THE ENEMY 0-0 (+0) 06:00:00 left']
@@ -43,6 +43,21 @@ describe Cinch::Plugins::TyrantHistory do
   describe 'flood control' do
     def msg
       make_message(bot, '!last', channel: '#test')
+    end
+
+    before :each do
+      @conn.respond('getOldFactionWars', nil, {'wars' => [
+        {
+          'faction_war_id' => '1',
+          'name' => 'THE ENEMY',
+          'attacker_faction_id' => '1000',
+          'defender_faction_id' => '1001',
+          'start_time' => Time.now.to_i.to_s,
+          'duration' => '6',
+          'atk_pts' => '0',
+          'def_pts' => '0',
+        },
+      ]})
     end
 
     it 'warns on second' do
