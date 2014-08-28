2764f353aa5317288108a8a4c4ab052dfbe4256f
Peter Tseng <pht24@cornell.edu>
2014-08-28 02:24:27 +0000

    spec/TyrantTile: Move all into a context
    So much repeated code to be removed

--- spec/tyrant-tile_spec.rb
+++ spec/tyrant-tile_spec.rb
@@ -37,99 +37,94 @@ describe Cinch::Plugins::TyrantTile do
     end
   end
 
-  describe "!tile on someone else's uncontested tile" do
+  describe '!tile' do
     before :each do
       @conn = FakeConnection.new
       @tyrant = Tyrants.get_fake('testplayer', @conn)
       expect(Tyrants).to receive(:get).with('testplayer').and_return(@tyrant)
     end
 
-    let(:message) { make_message(bot, '!tile 2', channel: '#test') }
+    describe "on someone else's uncontested tile" do
+      let(:message) { make_message(bot, '!tile 2', channel: '#test') }
 
-    it 'shows tile info' do
-      bot.plugins[0].stub(:shared).and_return({:conquest_map_hash => {
-        '1' => make_tile(1, 1000, x: 1, y: 2),
-        '2' => make_tile(2, 1001, x: 3, y: 4),
-        '3' => make_tile(3, 1002, x: 5, y: 6),
-      }})
-      replies = get_replies_text(message)
-      expect(replies).to be == [' 3E,  4S: faction 1001, CR: 1']
+      it 'shows tile info' do
+        bot.plugins[0].stub(:shared).and_return({:conquest_map_hash => {
+          '1' => make_tile(1, 1000, x: 1, y: 2),
+          '2' => make_tile(2, 1001, x: 3, y: 4),
+          '3' => make_tile(3, 1002, x: 5, y: 6),
+        }})
+        replies = get_replies_text(message)
+        expect(replies).to be == [' 3E,  4S: faction 1001, CR: 1']
+      end
+    end
+
+    describe 'on my uncontested tile' do
+      before :each do
+        bot.plugins[0].stub(:shared).and_return({:conquest_map_hash => {
+          '1' => make_tile(1, 1000, x: 1, y: 2),
+          '2' => make_tile(2, 1001, x: 3, y: 4),
+          '3' => make_tile(3, 1002, x: 5, y: 6),
+        }})
+      end
+
+      let(:message) { make_message(bot, '!tile 1', channel: '#test') }
+
+      it 'shows tile info on tile with decks' do
+        @conn.respond('getConquestTileInfo', 'system_id=1', {'system' => {
+          'max_health' => 100,
+          'slots' => {
+            '1' => {'commander_id' => 1000, 'health' => '100', 'defeated' => '0'},
+          },
+        }})
+        replies = get_replies_text(message)
+        expect(replies).to be == [
+          ' 1E,  2S: faction 1000, CR: 1',
+          '1/1 (100.00%) slots alive. 100/100 (100.00%) health left.'
+        ]
+      end
+
+      it 'shows tile info on tile with no decks' do
+        @conn.respond('getConquestTileInfo', 'system_id=1', {'system' => {
+          'max_health' => 100,
+          'slots' => {},
+        }})
+        replies = get_replies_text(message)
+        expect(replies).to be == [
+          ' 1E,  2S: faction 1000, CR: 1',
+          # TODO: Do we really want this kind of output?!
+          '0/0 (NaN%) slots alive. 0/0 (NaN%) health left.'
+        ]
+      end
+    end
+
+    describe 'on a tile we are defending' do
+      before :each do
+        bot.plugins[0].stub(:shared).and_return({:conquest_map_hash => {
+          '1' => make_tile(1, 1000, 1001, x: 1, y: 2),
+          '2' => make_tile(2, 1001, x: 3, y: 4),
+          '3' => make_tile(3, 1002, x: 5, y: 6),
+        }})
+      end
+
+      let(:message) { make_message(bot, '!tile 1', channel: '#test') }
+
+      it 'shows tile info on tile with no decks' do
+        @conn.respond('getConquestTileInfo', 'system_id=1', {'system' => {
+          'max_health' => 100,
+          'attacking_faction_id' => '1000',
+          'attack_start_time' => 900,
+          'attack_end_time' => 1500,
+          'slots' => {
+            '1' => {'commander_id' => 1000, 'health' => '100', 'defeated' => '0'},
+          },
+        }})
+        allow(Time).to receive(:now).and_return(1200)
+        replies = get_replies_text(message)
+        expect(replies).to be == [
+          ' 1E,  2S: faction 1000, CR: 1',
+          '1/1 (100.00%) slots alive. 100/100 (100.00%) health left. 00:05:00 (50.00%) left.'
+        ]
+      end
     end
   end
-
-  describe '!tile on my uncontested tile' do
-    before :each do
-      @conn = FakeConnection.new
-      @tyrant = Tyrants.get_fake('testplayer', @conn)
-      expect(Tyrants).to receive(:get).with('testplayer').and_return(@tyrant)
-      bot.plugins[0].stub(:shared).and_return({:conquest_map_hash => {
-        '1' => make_tile(1, 1000, x: 1, y: 2),
-        '2' => make_tile(2, 1001, x: 3, y: 4),
-        '3' => make_tile(3, 1002, x: 5, y: 6),
-      }})
-    end
-
-    let(:message) { make_message(bot, '!tile 1', channel: '#test') }
-
-    it 'shows tile info on tile with decks' do
-      @conn.respond('getConquestTileInfo', 'system_id=1', {'system' => {
-        'max_health' => 100,
-        'slots' => {
-          '1' => {'commander_id' => 1000, 'health' => '100', 'defeated' => '0'},
-        },
-      }})
-      replies = get_replies_text(message)
-      expect(replies).to be == [
-        ' 1E,  2S: faction 1000, CR: 1',
-        '1/1 (100.00%) slots alive. 100/100 (100.00%) health left.'
-      ]
-    end
-
-    it 'shows tile info on tile with no decks' do
-      @conn.respond('getConquestTileInfo', 'system_id=1', {'system' => {
-        'max_health' => 100,
-        'slots' => {},
-      }})
-      replies = get_replies_text(message)
-      expect(replies).to be == [
-        ' 1E,  2S: faction 1000, CR: 1',
-        # TODO: Do we really want this kind of output?!
-        '0/0 (NaN%) slots alive. 0/0 (NaN%) health left.'
-      ]
-    end
-  end
-
-  describe '!tile on a tile we are defending' do
-    before :each do
-      @conn = FakeConnection.new
-      @tyrant = Tyrants.get_fake('testplayer', @conn)
-      expect(Tyrants).to receive(:get).with('testplayer').and_return(@tyrant)
-      bot.plugins[0].stub(:shared).and_return({:conquest_map_hash => {
-        '1' => make_tile(1, 1000, 1001, x: 1, y: 2),
-        '2' => make_tile(2, 1001, x: 3, y: 4),
-        '3' => make_tile(3, 1002, x: 5, y: 6),
-      }})
-    end
-
-    let(:message) { make_message(bot, '!tile 1', channel: '#test') }
-
-    it 'shows tile info on tile with no decks' do
-      @conn.respond('getConquestTileInfo', 'system_id=1', {'system' => {
-        'max_health' => 100,
-        'attacking_faction_id' => '1000',
-        'attack_start_time' => 900,
-        'attack_end_time' => 1500,
-        'slots' => {
-          '1' => {'commander_id' => 1000, 'health' => '100', 'defeated' => '0'},
-        },
-      }})
-      allow(Time).to receive(:now).and_return(1200)
-      replies = get_replies_text(message)
-      expect(replies).to be == [
-        ' 1E,  2S: faction 1000, CR: 1',
-        '1/1 (100.00%) slots alive. 100/100 (100.00%) health left. 00:05:00 (50.00%) left.'
-      ]
-    end
-  end
-
 end
