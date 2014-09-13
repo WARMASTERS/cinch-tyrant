83d9a0a696b52f5155efa1cec8bf3c56ce8a3962
Peter Tseng <pht24@cornell.edu>
2014-09-13 16:34:16 +0000

    spec/TyrantStats: Move !stats into a describe
    Preparation for next commit

--- spec/tyrant-stats_spec.rb
+++ spec/tyrant-stats_spec.rb
@@ -24,24 +24,27 @@ describe Cinch::Plugins::TyrantStats do
     expect(bot).to be_a Cinch::Bot
   end
 
-  let(:message) { make_message(bot, '!stats', channel: '#test') }
+  describe '!stats' do
+    let(:message) { make_message(bot, '!stats', channel: '#test') }
 
-  it 'shows stats link to user' do
-    message.user.stub(:signed_on_at).and_return(0)
-    expect(IO).to receive(:read).with('/tmp/tyrant/stats_passwords/somefaction.txt').and_return('seekrit')
-    password = Digest::SHA1.hexdigest('test$seekrit')
-    replies = get_replies_text(message)
-    expect(replies).to be == [
-      'http://localhost/stats.php?faction=somefaction&username=test&password=' + password
-    ]
+    it 'shows stats link to user' do
+      message.user.stub(:signed_on_at).and_return(0)
+      expect(IO).to receive(:read).with('/tmp/tyrant/stats_passwords/somefaction.txt').and_return('seekrit')
+      password = Digest::SHA1.hexdigest('test$seekrit')
+      replies = get_replies_text(message)
+      expect(replies).to be == [
+        'http://localhost/stats.php?faction=somefaction&username=test&password=' + password
+      ]
+    end
+
+    it 'denies just-logged-in user' do
+      message.user.stub(:signed_on_at).and_return(Time.now.to_i)
+      replies = get_replies_text(message)
+      expect(replies).to be == [
+        'test: STAY A WHILE AND LISTEN! It is very rude to ask for !stats right after signing on.',
+        'http://localhost/stats.php?faction=somefaction&username=stayawhile&password=andlisten',
+      ]
+    end
   end
 
-  it 'denies just-logged-in user' do
-    message.user.stub(:signed_on_at).and_return(Time.now.to_i)
-    replies = get_replies_text(message)
-    expect(replies).to be == [
-      'test: STAY A WHILE AND LISTEN! It is very rude to ask for !stats right after signing on.',
-      'http://localhost/stats.php?faction=somefaction&username=stayawhile&password=andlisten',
-    ]
-  end
 end
